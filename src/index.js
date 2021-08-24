b9f20b46f6125135e9e0345e143484ceda84588a
Ratishjain12 <ratishjain6@gmail.com>
2021-08-24 18:28:24 +0530

    make it bette

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Auth0ProviderWithHistory from "./auth0-provider-with-history";
 
 ReactDOM.render(
-  <Auth0ProviderWithHistory>
+  <Router>
+    <Auth0ProviderWithHistory>
     <App />
-  </Auth0ProviderWithHistory>,
+   </Auth0ProviderWithHistory>
+  </Router>
+  
   document.getElementById("root")
 );
 // If you want to start measuring performance in your app, pass a function
