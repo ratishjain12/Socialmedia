9cb6217779548c35ac0bbdd4253d371d771ea0fd
Ratishjain12 <ratishjain6@gmail.com>
2021-08-24 18:20:37 +0530

    make it better

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,7 @@
 import React,{useEffect,useState }from 'react'
 import { useAuth0} from '@auth0/auth0-react'
 import {Image} from 'cloudinary-react'
-import Loading from '../../components/Loading'
+
 
 import Axios from 'axios'
 function Home() {
